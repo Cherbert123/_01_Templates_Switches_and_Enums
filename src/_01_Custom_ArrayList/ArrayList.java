ae41e93465bf6fe33c17164f1473f567c24e8023
Charlie <league@WTS14.attlocal.net>
2018-07-29 12:54:39 -0700

    CustomArrays

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,45 +2,66 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+
 	T[] tArray = (T[]) new Object[0];
-	
+
 	public ArrayList() {
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
 		return tArray[loc];
 	}
-	
+
 	public void add(T val) {
-		Object[] temp = new Object[ta.length];
-		 for (int j = 0; j < ta.length; j++) {
-			temp[j] = ta[j];
-		 }
-		 ta = (T[]) new Object[ta.length + 1];
-		 for (int j = 0; j < temp.length; j++) {
-				ta[j] = (T) temp[j];
-			 }
-		 ta[ta.length - 1] = (T) i;
-		 lengthT = ta.length;
-		
+		Object[] temp = new Object[tArray.length];
+		for (int j = 0; j < tArray.length; j++) {
+			temp[j] = tArray[j];
+		}
+		tArray = (T[]) new Object[tArray.length + 1];
+		for (int j = 0; j < temp.length; j++) {
+			tArray[j] = (T) temp[j];
+		}
+		tArray[tArray.length - 1] = (T) val;
+
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		Object[] temp = new Object[tArray.length + 1];
+		for (int i = 0; i < loc; i++) {
+			temp[i] = tArray[i];
+		}
+		temp[loc] = val;
+		for (int i = loc + 1; i < temp.length; i++) {
+			temp[i] = tArray[i - 1];
+		}
+		tArray = (T[]) temp;
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		tArray[loc] = val;
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+
+		T[] aRay = (T[]) new Object[tArray.length - 1];
+		for (int j = 0; j < loc; j++) {
+			aRay[j] = tArray[j];
+		}
+		int ctr = loc;
+		for (int j2 = loc + 1; j2 < tArray.length; j2++) {
+			aRay[ctr++] = tArray[j2];
+		}
+		tArray = aRay;
 	}
-	
+
 	public boolean contains(T val) {
-		
-		return false;
+		boolean contain = false;
+		for (int i = 0; i < tArray.length; i++) {
+			if (tArray[i] == val) {
+				contain = true;
+			}
+		}
+		return contain;
 	}
 }
